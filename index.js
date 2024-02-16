36b4bc67aea4d762516c5a751a23bee9ac2ab8fb
prashantism2209 <prashantism2209@gmail.com>
2024-02-17 00:28:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ const dealsData = [];
   const page = await browser.newPage();
   const cookie = {
     name: 'sentinel_token',
-    value: '', //sentinel token without last semicolon
+    value: 'eyJhbGciOiJSUzI1NiIsImtpZCI6InNlbnRpbmVsL3h1OEM5MERLbUtsS3Y5VmZUU1JRM2pkS2RITFNMSlpveVRYckNFNWc0b3M9In0.eyJhdWQiOlsic2VudGluZWwiLCJESU5FT1VULURFQUwtQ1JNIl0sImV4cCI6MTcwODE5NDA1OSwiaXNzIjoic2VudGluZWwiLCJuYW1lIjoiUHJhc2hhbnQgS3VtYXIiLCJuYmYiOjE3MDgxMDc2NTksIm9pZCI6ImI1MDRiYTRmLTQ3ZmQtNDQwYy05YjY2LTdkYTU1NTA3YjhjZCIsInJvbGVzIjpbIkFETUlOIl0sInN1YiI6InByYXNoYW50Lmt1bWFyNkBzd2lnZ3kuaW4ifQ.RFx32wiY1lQf8tftuDhNn1scefDapOVx6fc0hgZSPQzeG4b6mEWl3xe_Bel3WzyJEzPIQTGXlYiQIGRn3Gz0k5MZEX3NQ4FLHE6BvbGXMSH4UG2Dh8w25skGScBPwAf8tndvUjd6sOE7ysXiJdpTnqYnC_nt7zJ9f5NTGkf4paXJ3LTyHhbqvbC7iClGwO652v4vPtz6zjSJ7by_gDgENaQhCN6gE1wp7teoTnr9KZ7cX3RH6Zs5h9F-wrtDy_MVTAUZ94kjM8Wj_w7eJbELveBzhWTVRmB91tCsVgVWbk8tvuZIHehEqd8OO-NNzXaTtRKbN4RwI-AParOmirZYVg', //sentinel token without last semicolon
     domain: 'dineout-girf-deals-dashboard.swiggyops.de',
     url: 'https://dineout-girf-deals-dashboard.swiggyops.de',
     path: '/',
@@ -46,13 +46,19 @@ const dealsData = [];
     await page.goto("https://dineout-girf-deals-dashboard.swiggyops.de/admin/TicketCreation/deal_edit?r_id="+deal.R_ID+"&tl_id="+deal.TL_ID, {
       waitUntil: "domcontentloaded",
     });
- 
-    await page.evaluate(() => {
-      document.querySelector("#min_quantity").value=1;
-    });
-      
+  
+
+
+// await page.evaluate(  () => {
+//   console.log("inside evaluate");
+//   document.querySelector("#min_quantity").value=1;
+// });
+    await page.waitForSelector('input[name=min_quantity]');
+    await page.$eval('#min_quantity', el => el.value = '1');
     await page.click('#save_btn');
+    console.log("clicked save");
     await page.waitForNavigation();
- }
+  }
+
   // // Close the browser
    await browser.close();
